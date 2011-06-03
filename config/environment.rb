bd6eb975db18d72f0166615618dd014701cd8c36
Hardik <hardikshah@gatech.edu>
2011-06-03 19:59:27 -0400

    Final commit

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 Haml::Template.options[:format] = :html5
 
 # Initialize the rails application
-Twopickles::Application.initialize!
+FirstApp::Application.initialize!
