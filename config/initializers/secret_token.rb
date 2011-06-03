3b966bf9055584bf579aaa942c2807f76290431f
Hardik <hardikshah@gatech.edu>
2011-06-03 19:43:00 -0400

    Final commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'c89e97ecf8ac5014203e324414281cea692e54b76e1a7044266a1d61023d36b505012483ebfa91a087fb1c0912d8b2292abecb8597496d871e0fdad21964c534'
+Twopickles::Application.config.secret_token = 'd716b696bb3592d1fd94a86417a96331cb03e22aa929afbfc61fe6512d1a834ba519a70772272f3ccf27ffe803f2d5191725b5c071f38db88c5499e227c081d1'
